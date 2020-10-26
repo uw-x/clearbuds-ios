f52229e2cb616b199a12ebb54dc79258b33e9b16
Maruchi Kim <maruchi.kim@gmail.com>
2020-10-26 11:55:20 -0700

    Audio file upload to S3 working

---
+++ shio-dc/AWSS3Manager.swift
@@ -0,0 +1,113 @@
+//
+//  AWSS3Manager.swift
+//  shio-dc
+//
+//  Created by Maruchi Kim on 10/26/20.
+//
+
+import Foundation
+import UIKit
+import AWSS3 //1
+
+typealias progressBlock = (_ progress: Double) -> Void //2
+typealias completionBlock = (_ response: Any?, _ error: Error?) -> Void //3
+
+class AWSS3Manager {
+    
+    static let shared = AWSS3Manager() // 4
+    private init () { }
+    let bucketName = "shio-data" //5
+    
+    // Upload image using UIImage object
+    func uploadImage(image: UIImage, progress: progressBlock?, completion: completionBlock?) {
+        
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+            let error = NSError(domain:"", code:402, userInfo:[NSLocalizedDescriptionKey: "invalid image"])
+            completion?(nil, error)
+            return
+        }
+        
+        let tmpPath = NSTemporaryDirectory() as String
+        let fileName: String = ProcessInfo.processInfo.globallyUniqueString + (".jpeg")
+        let filePath = tmpPath + "/" + fileName
+        let fileUrl = URL(fileURLWithPath: filePath)
+        
+        do {
+            try imageData.write(to: fileUrl)
+            self.uploadfile(fileUrl: fileUrl, fileName: fileName, contenType: "image", progress: progress, completion: completion)
+        } catch {
+            let error = NSError(domain:"", code:402, userInfo:[NSLocalizedDescriptionKey: "invalid image"])
+            completion?(nil, error)
+        }
+    }
+    
+    // Upload video from local path url
+    func uploadVideo(videoUrl: URL, progress: progressBlock?, completion: completionBlock?) {
+        let fileName = self.getUniqueFileName(fileUrl: videoUrl)
+        self.uploadfile(fileUrl: videoUrl, fileName: fileName, contenType: "video", progress: progress, completion: completion)
+    }
+    
+    // Upload auido from local path url
+    func uploadAudio(audioUrl: URL, progress: progressBlock?, completion: completionBlock?) {
+        let fileName = self.getUniqueFileName(fileUrl: audioUrl)
+        self.uploadfile(fileUrl: audioUrl, fileName: fileName, contenType: "audio", progress: progress, completion: completion)
+    }
+    
+    // Upload files like Text, Zip, etc from local path url
+    func uploadOtherFile(fileUrl: URL, conentType: String, progress: progressBlock?, completion: completionBlock?) {
+        let fileName = self.getUniqueFileName(fileUrl: fileUrl)
+        self.uploadfile(fileUrl: fileUrl, fileName: fileName, contenType: conentType, progress: progress, completion: completion)
+    }
+    
+    // Get unique file name
+    func getUniqueFileName(fileUrl: URL) -> String {
+        let strExt: String = "." + (URL(fileURLWithPath: fileUrl.absoluteString).pathExtension)
+        return (ProcessInfo.processInfo.globallyUniqueString + (strExt))
+    }
+    
+    //MARK:- AWS file upload
+    // fileUrl :  file local path url
+    // fileName : name of file, like "myimage.jpeg" "video.mov"
+    // contenType: file MIME type
+    // progress: file upload progress, value from 0 to 1, 1 for 100% complete
+    // completion: completion block when uplaoding is finish, you will get S3 url of upload file here
+    private func uploadfile(fileUrl: URL, fileName: String, contenType: String, progress: progressBlock?, completion: completionBlock?) {
+        // Upload progress block
+        let expression = AWSS3TransferUtilityUploadExpression()
+        expression.progressBlock = {(task, awsProgress) in
+            guard let uploadProgress = progress else { return }
+            DispatchQueue.main.async {
+                uploadProgress(awsProgress.fractionCompleted)
+            }
+        }
+        // Completion block
+        var completionHandler: AWSS3TransferUtilityUploadCompletionHandlerBlock?
+        completionHandler = { (task, error) -> Void in
+            DispatchQueue.main.async(execute: {
+                if error == nil {
+                    let url = AWSS3.default().configuration.endpoint.url
+                    let publicURL = url?.appendingPathComponent(self.bucketName).appendingPathComponent(fileName)
+                    print("Uploaded to:\(String(describing: publicURL))")
+                    if let completionBlock = completion {
+                        completionBlock(publicURL?.absoluteString, nil)
+                    }
+                } else {
+                    if let completionBlock = completion {
+                        completionBlock(nil, error)
+                    }
+                }
+            })
+        }
+        // Start uploading using AWSS3TransferUtility
+        let awsTransferUtility = AWSS3TransferUtility.default()
+        awsTransferUtility.uploadFile(fileUrl, bucket: bucketName, key: fileName, contentType: contenType, expression: expression, completionHandler: completionHandler).continueWith { (task) -> Any? in
+            if let error = task.error {
+                print("error is: \(error.localizedDescription)")
+            }
+            if let _ = task.result {
+                // your uploadTask
+            }
+            return nil
+        }
+    }
+}
--- shio-dc/ViewController.swift
+++ shio-dc/ViewController.swift
@@ -3,17 +3,39 @@
 //  shio-dc
 //
 //  Created by Maruchi Kim on 10/26/20.
-//
+//  https://www.swiftdevcenter.com/upload-image-video-audio-and-any-type-of-files-to-aws-s3-bucket-swift/
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var s3UrlLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    let testFilePath = Bundle.main.path(forResource: "Two Dumbs Up - uncoolclub", ofType: "mp3")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func tapUploadAudio(_ sender: Any) {
+        let audioUrl = URL(fileURLWithPath: testFilePath!)
+            AWSS3Manager.shared.uploadAudio(audioUrl: audioUrl, progress: { [weak self] (progress) in
+                
+                guard let strongSelf = self else { return }
+                strongSelf.progressView.progress = Float(progress)
+                
+            }) { [weak self] (uploadedFileUrl, error) in
+                
+                guard let strongSelf = self else { return }
+                if let finalPath = uploadedFileUrl as? String {
+                    strongSelf.s3UrlLabel.text = "Uploaded file url: " + finalPath
+                } else {
+                    print("\(String(describing: error?.localizedDescription))")
+                }
+            }
+    }
+    
 }
 
